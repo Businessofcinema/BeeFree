779fa692fe347b27435dd95dd62e12ecb4771807
Ameer Ahmed <ameer@clara.doctor>
2023-05-24 07:04:17 -0400

    update server url

--- client/components/_constants.tsx
+++ client/components/_constants.tsx
@@ -1,2 +1,2 @@
-// export const SERVER_URL: string = "https://beefreecommunity.org"
-export const SERVER_URL: string = 'http://localhost:3001';
+export const SERVER_URL: string = 'https://beefreecommunity.net';
+// export const SERVER_URL: string = 'http://localhost:3001';
